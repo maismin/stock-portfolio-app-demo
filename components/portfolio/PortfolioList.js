53eed8991176873a6b1978ee294a6cfbcd5026cc
Simon Mai <maismin@gmail.com>
2019-10-02 19:31:27 -0400

    Create components for /portfolio page
    - Create PortfolioList component to display user's stock portfolio
    - Create StockForm component that allows user to buy and sell stocks

---
+++ components/portfolio/PortfolioList.js
@@ -0,0 +1,53 @@
+import { Header, Table, Segment } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+const performance = value => {
+  const color = {};
+  if (value < 0) {
+    color.color = 'red';
+  } else if (value === 0) {
+    color.color = 'grey';
+  } else {
+    color.color = 'green';
+  }
+  return color;
+};
+
+/* eslint-disable no-underscore-dangle, react/jsx-one-expression-per-line */
+const mapStocksToRows = stocks => {
+  return stocks.map(s => {
+    const color = performance(s.performance);
+    return (
+      <Table.Row key={s._id} style={{ size: '5em' }}>
+        <Table.Cell style={color}>
+          <span>{s.ticker}</span>
+        </Table.Cell>
+        <Table.Cell>{`${s.shares} shares`}</Table.Cell>
+        <Table.Cell style={color}>{`$ ${s.value}`}</Table.Cell>
+      </Table.Row>
+    );
+  });
+};
+
+const PortfolioList = ({ portfolio }) => {
+  return (
+    <>
+      <Segment>
+        <Header size="huge">Portfolio - ${portfolio.value}</Header>
+      </Segment>
+      <Table>
+        <Table.Body>{mapStocksToRows(portfolio.stocks)}</Table.Body>
+      </Table>
+    </>
+  );
+};
+/* eslint-enable no-underscore-dangle, react/jsx-one-expression-per-line */
+
+PortfolioList.propTypes = {
+  portfolio: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    stocks: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+export default PortfolioList;
---
+++ components/portfolio/StockForm.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types';
+import { Button, Container, Form, Message, Segment } from 'semantic-ui-react';
+
+const StockForm = ({
+  stock,
+  loading,
+  disabled,
+  error,
+  transactionOptions,
+  handleChange,
+  handleSubmit,
+}) => (
+  <Container>
+    <Form loading={loading} error={Boolean(error)} onSubmit={handleSubmit}>
+      <Message error header="Oops!" content={error} />
+      <Segment>
+        <Form.Input
+          fluid
+          label="Ticker"
+          placeholder="Ticker"
+          name="ticker"
+          type="text"
+          value={stock.ticker}
+          onChange={handleChange}
+        />
+        <Form.Input
+          fluid
+          label="Shares"
+          placeholder="Shares"
+          name="shares"
+          type="number"
+          min="1"
+          value={stock.shares}
+          onChange={handleChange}
+        />
+        <Form.Dropdown
+          placeholder="Select an action"
+          fluid
+          selection
+          options={transactionOptions}
+          onChange={handleChange}
+        />
+        <Button
+          disabled={disabled || loading}
+          fluid
+          type="submit"
+          name="action"
+          color="blue"
+          content="Submit"
+        />
+      </Segment>
+    </Form>
+  </Container>
+);
+
+StockForm.propTypes = {
+  stock: PropTypes.shape({
+    action: PropTypes.string.isRequired,
+    ticker: PropTypes.string.isRequired,
+    shares: PropTypes.string.isRequired,
+  }).isRequired,
+  loading: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  transactionOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default StockForm;
