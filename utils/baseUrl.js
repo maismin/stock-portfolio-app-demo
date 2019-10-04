8a03ff9cf613b568268b6e1b15c3746672b54554
Simon Mai <maismin@gmail.com>
2019-10-03 20:46:38 -0400

    Fix baseUrl for production

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'productionURL'
+    ? 'https://stock-portfolio-ttp-demo.herokuapp.com'
     : 'http://localhost:3000';
 
 export default baseUrl;
