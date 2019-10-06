7a837861fe1fe6bbdc269bd8873cea11e7bd35b8
Simon Mai <maismin@gmail.com>
2019-10-06 18:19:16 -0400

    Add E2E test for logging in

---
+++ cypress/e2e/login_spec.js
@@ -0,0 +1,29 @@
+describe('login', () => {
+  const user = {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: '123456',
+  };
+
+  beforeEach(() => {
+    cy.resetDb();
+  });
+
+  it('should login a new user', () => {
+    cy.server();
+    cy.route('/api/portfolio').as('getPortfolio');
+
+    cy.createUser(user);
+    cy.visit('/login')
+      .get('input[name=email')
+      .type(user.email)
+      .get('input[name=password')
+      .type(user.password)
+      .get('button[type=submit')
+      .click();
+
+    cy.wait('@getPortfolio');
+    cy.assertPortfolio();
+    cy.assertLoggedIn();
+  });
+});
